c084ef0eac853c88d291d6b18ae23ea624c86ced
Jason Chen <july8jason@gmail.com>
2024-05-27 14:49:54 -0400

    add home page

--- website/views.py
+++ website/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from ..data_scripts.helpers import get_top_5_stocks_by_marketcap
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html', {})
