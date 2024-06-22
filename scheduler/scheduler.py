1df8eec1d2e38bdf171adb22c4c0e6545ed2eca1
Jason <july8jason@gmail.com>
2024-06-22 14:42:38 -0400

    delete batch scheduler

--- scheduler/scheduler.py
+++
@@ -1,6 +0,0 @@
-from requests import post
-
-if __name__ == '__main__':
-    print("Sending request to start top 5 scraper...")
-    response = post('http://localhost:5000/start-top-5-scraper')
-    print(response.json())
